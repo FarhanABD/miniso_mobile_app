737a76fc30611f40d063d448e7d249e29072b02f
FarhanABD <farhanabdilah204@gmail.com>
2024-01-13 19:01:06 +0700

    Commit finish Place order

--- lib/minor_screen/place_order.dart
+++ lib/minor_screen/place_order.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:miniso_store/providers/cart_provider.dart';
 import 'package:miniso_store/widgets/appbar_widget.dart';
 import 'package:miniso_store/widgets/yellow_button.dart';
+import 'package:provider/provider.dart';
 
 class PlaceOrderScreen extends StatefulWidget {
   const PlaceOrderScreen({Key? key}) : super(key: key);
@@ -30,6 +32,14 @@ class _PlaceOrderScreenState extends State<PlaceOrderScreen> {
             return const Text("Document does not exist");
           }
 
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Material(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
           if (snapshot.connectionState == ConnectionState.done) {
             Map<String, dynamic> data =
                 snapshot.data!.data() as Map<String, dynamic>;
@@ -50,9 +60,25 @@ class _PlaceOrderScreenState extends State<PlaceOrderScreen> {
                       children: [
                         Container(
                           height: 90,
+                          width: double.infinity,
                           decoration: BoxDecoration(
                               color: Colors.white,
                               borderRadius: BorderRadius.circular(15)),
+                          child:
+                              //--------- MENAMPILKAN DATA ORDER USER ------------//
+                              Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 4, horizontal: 16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text('Name: ${data['name']}'),
+                                Text('Phone: ${data['phone']}'),
+                                Text('Address: ${data['address']}'),
+                              ],
+                            ),
+                          ),
                         ),
                         const SizedBox(
                           height: 20,
@@ -62,6 +88,101 @@ class _PlaceOrderScreenState extends State<PlaceOrderScreen> {
                             decoration: BoxDecoration(
                                 color: Colors.white,
                                 borderRadius: BorderRadius.circular(15)),
+                            child: Consumer<Cart>(
+                              builder: (context, cart, child) {
+                                return ListView.builder(
+                                    itemCount: cart.count,
+                                    itemBuilder: (context, index) {
+                                      final order = cart.getItems[index];
+                                      return Padding(
+                                        padding: const EdgeInsets.all(6.0),
+                                        child: Container(
+                                          height: 100,
+                                          decoration: BoxDecoration(
+                                              border: Border.all(width: 0.3),
+                                              borderRadius:
+                                                  BorderRadius.circular(15)),
+                                          child: Row(
+                                            children: [
+                                              ClipRRect(
+                                                borderRadius:
+                                                    const BorderRadius.only(
+                                                        topLeft:
+                                                            Radius.circular(15),
+                                                        bottomLeft:
+                                                            Radius.circular(
+                                                                15)),
+                                                child: SizedBox(
+                                                  height: 100,
+                                                  width: 100,
+                                                  //---- MENAMPILKAN GAMBAR PRODUK YANG DI CHECKOUT -----//
+                                                  child: Image.network(
+                                                      order.imagesUrl.first),
+                                                ),
+                                              ),
+                                              Flexible(
+                                                child: Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceAround,
+                                                  children: [
+                                                    Text(
+                                                      order.name,
+                                                      maxLines: 2,
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                      style: TextStyle(
+                                                          fontSize: 16,
+                                                          fontWeight:
+                                                              FontWeight.w600,
+                                                          color: Colors
+                                                              .grey.shade600),
+                                                    ),
+                                                    Padding(
+                                                      padding: const EdgeInsets
+                                                              .symmetric(
+                                                          vertical: 4,
+                                                          horizontal: 12),
+                                                      child: Row(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .spaceBetween,
+                                                        children: [
+                                                          Text(
+                                                            order.price
+                                                                .toStringAsFixed(
+                                                                    2),
+                                                            style: const TextStyle(
+                                                                fontSize: 16,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w600,
+                                                                color:
+                                                                    Colors.red),
+                                                          ),
+                                                          Text(
+                                                            'X  ${order.qty.toString()}',
+                                                            style: const TextStyle(
+                                                                fontSize: 16,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w600,
+                                                                color:
+                                                                    Colors.red),
+                                                          )
+                                                        ],
+                                                      ),
+                                                    )
+                                                  ],
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    });
+                              },
+                            ),
                           ),
                         ),
                       ],
