fc5dba8b7aeba81db687dcee28a11f1e8273c359
FarhanABD <farhanabdilah204@gmail.com>
2024-02-04 09:17:56 +0700

    streaming store statics

--- lib/dashboard_components/suppliers_statics.dart
+++ lib/dashboard_components/suppliers_statics.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:miniso_store/widgets/appbar_widget.dart';
 
@@ -6,29 +8,61 @@ class StaticsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const AppBarTitle(
-          title: 'Static Screen',
-        ),
-        leading: const AppBarBackButton(),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            StaticsModel(label: 'sold out', value: 45),
-            StaticsModel(label: 'item count', value: 45),
-            StaticsModel(label: 'total balance', value: 45),
-            SizedBox(
-              height: 20,
-            )
-          ],
-        ),
-      ),
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('orders')
+            .where('sid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Material(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          //--------- FUNCTION UTUK MENAMPILKAN JUMLAH PESANAN ---------------//
+          num itemCount = 0;
+          for (var item in snapshot.data!.docs) {
+            itemCount += item['orderqty'];
+          }
+
+          //----------- FUNCTION UNTUK MENAMPILKA TOTAL PENDAPATAN -----------//
+          double totalPrice = 0.0;
+          for (var item in snapshot.data!.docs) {
+            totalPrice += item['orderqty'] * item['orderprice'];
+          }
+
+          return Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              title: const AppBarTitle(
+                title: 'Static Screen',
+              ),
+              leading: const AppBarBackButton(),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  StaticsModel(
+                      label: 'sold out', value: snapshot.data!.docs.length),
+                  StaticsModel(label: 'item count', value: itemCount),
+                  StaticsModel(label: 'total balance', value: totalPrice),
+                  const SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
 
@@ -65,10 +99,10 @@ class StaticsModel extends StatelessWidget {
                   bottomLeft: Radius.circular(15),
                   bottomRight: Radius.circular(15))),
           child: Center(
-            child: Text(value.toUpperCase(),
+            child: Text(value.toString(),
                 style: const TextStyle(
                     color: Colors.pink,
-                    fontSize: 20,
+                    fontSize: 24,
                     fontFamily: 'Acme',
                     letterSpacing: 2,
                     fontWeight: FontWeight.bold)),
